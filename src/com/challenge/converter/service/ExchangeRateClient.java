9df0795dd123d67a71af874cf36975a058516386
Gustavo Rios <gerquevedo@unsm.edu.pe>
2025-05-18 00:50:23 -0500

    Construyendo la Solicitud (HttpRequest)

--- src/com/challenge/converter/service/ExchangeRateClient.java
+++ src/com/challenge/converter/service/ExchangeRateClient.java
@@ -5,8 +5,18 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.URI;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 public class ExchangeRateClient {
+
+    private final HttpClient client;
+
+    public ExchangeRateClient() {
+        this.client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(10)) //Tiempo máximo de espera 10 seg.
+                .build();
+    }
+
     public String obtenerJson(String baseCurrency) {
         String apiKey = "f334ac38203a36d22057701f";
         String url = "https://v6.exchangerate-api.com/v6/"+apiKey+"/latest/"+baseCurrency;
@@ -14,11 +24,20 @@ public class ExchangeRateClient {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
+                .timeout(Duration.ofSeconds(10)) //Tiempo de espera para la solicitud
+                .header("User-Agent", "Java Conversor de moneda - Challenge") // Buenas prácticas
+                .GET()
                 .build();
 
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            if (response.statusCode() == 200) {
+                return response.body();
+            } else {
+                System.out.println("Error: código de estado HTTP: " + response.statusCode());
+                return null;
+            }
+
         } catch (IOException | InterruptedException e) {
             System.out.println("Erro al obtener datos de la API: "+e.getMessage());
             return null;
